ca1bae61ba67df8cf524791cc7b471328e2cc9f2
kemish <kemish.hendershot@gmail.com>
2017-11-29 19:44:40 -0700

    Updated handlebars vulnerability
    Signed-off-by: kemish <kemish.hendershot@gmail.com>

--- src/app/_directives/alert.component.ts
+++ src/app/_directives/alert.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertService } from '../_services/index';
+// import { AlertService } from '../_services/index';
 
 @Component({
     moduleId: module.id,
@@ -9,9 +9,9 @@ import { AlertService } from '../_services/index';
 export class AlertComponent implements OnInit {
     message: any;
 
-    constructor(private alertService: AlertService) {}
+    constructor() {}
 
     ngOnInit() {
-        this.alertService.getMessage().subscribe(message => { this.message = message; });
+        // this.alertService.getMessage().subscribe(message => { this.message = message; });
     }
 }
